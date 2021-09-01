35111ca60864c3ecf25131c6f656e5d66965cf01
Halina <halina.syposz@gmail.com>
2021-09-01 19:30:51 +0200

    about me and basic contact section added

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 
-const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('skills-row-animation');
-        }
-    });
-});
+// const observer = new IntersectionObserver(entries => {
+//     entries.forEach(entry => {
+//         if (entry.isIntersecting) {
+//             entry.target.classList.add('skills-row-animation');
+//         }
+//     });
+// });
 
-observer.observe(document.querySelector('skills-row'));
+// observer.observe(document.querySelector('skills-row'));
