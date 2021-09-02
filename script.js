520865b0f7b2859555b4a80b728728773fe1c5a9
Halina <halina.syposz@gmail.com>
2021-09-02 15:31:00 +0200

    form and basic validation provided

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
+alert('dziala');
 
-// const observer = new IntersectionObserver(entries => {
-//     entries.forEach(entry => {
-//         if (entry.isIntersecting) {
-//             entry.target.classList.add('skills-row-animation');
-//         }
-//     });
-// });
+const form = document.getElementById('form');
+const firstName = document.getElementById("firstName");
+const surname = document.getElementById('surname');
+const email = document.getElementById('email');
+const phone = document.getElementById('phone');
+const message = document.getElementById('message');
+const errorElement = document.getElementById('error');
 
-// observer.observe(document.querySelector('skills-row'));
+form.addEventListener('submit', (event) => {
+    let errorMsgs = [];
+    
+    if (firstName.value == '' || firstName.value == null) {
+        errorMsgs.push("Name is required");
+    }
+
+    if (errorMsgs.length > 0) {
+        event.preventDefault();
+        errorElement.innerText = errorMsgs.join(', ');
+    }
+})
