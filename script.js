9214b02501bb4c369606c857a97188f879696b05
Halina <halina.syposz@gmail.com>
2021-09-02 19:01:28 +0200

    form full validation added

--- script.js
+++ script.js
@@ -1,22 +1,44 @@
-alert('dziala');
-
 const form = document.getElementById('form');
 const firstName = document.getElementById("firstName");
 const surname = document.getElementById('surname');
 const email = document.getElementById('email');
 const phone = document.getElementById('phone');
 const message = document.getElementById('message');
-const errorElement = document.getElementById('error');
+const errorFirstName = document.getElementById('error-firstName');
+const errorSurname = document.getElementById('error-surname');
+const errorEmail = document.getElementById('error-email');
 
 form.addEventListener('submit', (event) => {
-    let errorMsgs = [];
+    let errorFirstNameMessage = [];
+    let errorSurnameMessage = [];
+    let errorEmailMessage = [];
     
     if (firstName.value == '' || firstName.value == null) {
-        errorMsgs.push("Name is required");
+        errorFirstNameMessage.push("* Name is required");
     }
 
-    if (errorMsgs.length > 0) {
+    if (surname.value == '' || surname.value == null) {
+        errorSurnameMessage.push("* Surname is required");
+    }
+
+    const validMail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    if (!email.value.match(validMail)) {
+        errorEmailMessage.push("* Please enter a valid email");
+    }
+
+    if (errorFirstNameMessage.length > 0) {
         event.preventDefault();
-        errorElement.innerText = errorMsgs.join(', ');
+        errorFirstName.innerText = errorFirstNameMessage;
+    }
+
+    if (errorSurnameMessage.length > 0) {
+        event.preventDefault();
+        errorSurname.innerText = errorSurnameMessage;
+    }
+
+    if (errorEmailMessage.length > 0) {
+        event.preventDefault();
+        errorEmail.innerText = errorEmailMessage;
     }
 })
